731ecb6f3f3498f42081e6d7aded065e19c05a64
Xaver Pinero <xaver.pinero@gmail.com>
2020-12-07 11:53:35 -0500

    setup axios call

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from axios;
 import './App.css';
 
 function App() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {},
+      followers: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/xpinero')
+      .then(res => {
+         this.setState({
+          ...this.state,
+             user: res.data
+         });
+      })
+      .catch((err) => console.log('error', err));
+
+    axios
+      .get('https://api.github.com/users/xpinero/followers')
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          followers: Response.data
+        });
+      })
+      .catch((err) => console.log('error', err));
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
     </div>
   );
 }
